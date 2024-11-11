d6780d1d1daa2da3727782ec8713a171b1333ef5
udaychopade27 <udaychopade27@gmail.com>
2024-11-11 16:58:38 +0530

    update code for three tier app

--- ThreeTierApp/backend/index.js
+++ ThreeTierApp/backend/index.js
@@ -3,11 +3,15 @@ const connection = require("./db");
 const cors = require("cors");
 const express = require("express");
 const app = express();
-
 connection();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  // origin: '*' ,
+  // methods: 'GET, POST, PUT, DELETE, OPTIONS',
+  // allowedHeaders: 'Content-Type, Authorization'
+));
+
 
 app.get('/ok', (req, res) => {
     res.status(200).send('ok')
